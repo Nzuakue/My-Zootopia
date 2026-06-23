10171655186610da1778ca2f9193e7df15af2275
Diogni <45627313+Nzuakue@users.noreply.github.com>
2026-06-23 13:19:06 +0200

    generate card item for every animal

--- animals_attributes.py
+++ animals_attributes.py
@@ -16,11 +16,15 @@ def get_animals_attributes():
     fox_attributes = ""
 
     for fox_data in foxes_data:
-        fox_attributes += f"Name: {fox_data.get('name')} \n" if 'name' in fox_data.keys() else ""
-        fox_attributes += f"Diet: {fox_data.get('characteristics', {}).get('diet')} \n" if 'diet' in fox_data.get(
+        fox_attributes += "<li class=\"cards__item\">\n"
+        fox_attributes += f"Name: {fox_data.get('name')}<br/> \n" if 'name' in fox_data.keys() else ""
+        fox_attributes += f"Diet: {fox_data.get('characteristics', {}).get('diet')}<br/>\n" if 'diet' in fox_data.get(
             'characteristics', {}).keys() else ""
-        fox_attributes += f"Location: {fox_data.get('locations')[0]} \n" if 'locations' in fox_data.keys() else ""
-        fox_attributes += f"Type: {fox_data.get('characteristics', {}).get('type')} \n\n" if "type" in fox_data.get(
+        fox_attributes += f"Location: {fox_data.get('locations')[0]}<br/>\n" if 'locations' in fox_data.keys() else ""
+        fox_attributes += f"Type: {fox_data.get('characteristics', {}).get('type')}<br/>\n" if "type" in fox_data.get(
             'characteristics', {}).keys() else "\n"
+        fox_attributes += "</li>\n\n"
 
     return fox_attributes
+
+
